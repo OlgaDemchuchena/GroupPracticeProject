0b07c5e8d51097e8ac68284633c7753dd6c48a0f
OlgaDemchuchena <olha.shemiakina@gmail.com>
2023-05-20 23:17:13 -0700

    created divide without operator method

---
+++ src/utils/DivideWithoutOp.java
@@ -0,0 +1,17 @@
+package utils;
+
+public class DivideWithoutOp {
+    public static void divisionNoOp(int a, int b){
+
+
+       int x = 0;
+
+         while(a >= b){
+           a=a-b;
+           x++;
+       }
+
+           // System.out.println(x);
+
+    }
+}

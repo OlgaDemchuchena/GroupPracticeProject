feaf29c901b213b6ef6df910aa1304a8e7c4d876
OlgaDemchuchena <olha.shemiakina@gmail.com>
2023-05-20 23:18:11 -0700

    created multiply of 3 & 5 method

---
+++ src/utils/multiple35.java
@@ -0,0 +1,17 @@
+package utils;
+
+public class multiple35 {
+    public static void finra (int a) {
+
+        if(a % 3 == 0 && a % 5 == 0){
+            System.out.println("FINRA");
+        } else if (a % 3 == 0) {
+            System.out.println("FIN");
+
+        } else if (a % 5 == 0) {
+            System.out.println("RA");
+
+        }
+
+    }
+}

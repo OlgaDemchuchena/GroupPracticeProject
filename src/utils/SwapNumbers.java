2d8dfd01e9ef10c5dbf682ebe450f681c1d07c1a
OlgaDemchuchena <olha.shemiakina@gmail.com>
2023-06-01 11:54:14 -0700

    created swap numbers code

---
+++ src/utils/SwapNumbers.java
@@ -0,0 +1,15 @@
+package utils;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+
+        int a = 5, b = 3;
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println(a +", "+ b);
+        System.out.println(""+ a +" "+ b);
+    }
+}
